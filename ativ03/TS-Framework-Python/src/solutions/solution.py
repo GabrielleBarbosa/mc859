8ab21dc302416c4f90f87a9b94be8e872165b22d
Gabrielle Barbosa <g233862@dac.unicamp.br>
2025-09-18 21:49:06 -0300

    fix solution cost

--- ativ03/TS-Framework-Python/src/solutions/solution.py
+++ ativ03/TS-Framework-Python/src/solutions/solution.py
@@ -1,9 +1,14 @@
 import math
 
 class Solution(list):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cost = float('inf')
+    def __init__(self, solution = None, *args, **kwargs):
+        if solution != None :
+            super().__init__(solution)
+            self.cost = solution.cost
+        else :
+            super().__init__(*args, **kwargs)
+            self.cost = float('inf')
+        
 
     def __str__(self):
         return f"Solution: cost=[{self.cost}], size=[{len(self)}], elements={super().__str__()}"
