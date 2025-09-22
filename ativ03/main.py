f8f5b500a791274c14e95b2800af61443f94d234
Gabrielle Barbosa <g233862@dac.unicamp.br>
2025-09-21 22:05:03 -0300

    implement sc_qbf basic

--- ativ03/main.py
+++ ativ03/main.py
@@ -5,14 +5,14 @@ import sys
 # Add the project root to the Python path
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from src.problems.qbf.solvers.ts_qbf import TS_QBF
+from src.problems.sc_qbf.solvers.ts_sc_qbf import TS_SC_QBF
 
 def main():
     # Get the absolute path to the instance file
-    instance_file = os.path.join(os.path.dirname(__file__), 'instances', 'qbf', 'qbf100')
+    instance_file = os.path.join(os.path.dirname(__file__), 'instances', 'sc_qbf', 'scqbf_025_1')
     
     start_time = time.time()
-    tabusearch = TS_QBF(20, 1000, instance_file)
+    tabusearch = TS_SC_QBF(20, 1000, instance_file)
     best_sol = tabusearch.solve()
     print("maxVal =", best_sol)
     end_time = time.time()
---
+++ ativ03/src/problems/sc_qbf/solvers/ts_sc_qbf.py
@@ -0,0 +1,111 @@
+import sys
+import os
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+from collections import deque
+from src.metaheuristics.tabusearch.abstract_ts import AbstractTS
+from src.problems.sc_qbf.sc_qbf_inverse import SC_QBF_Inverse
+from src.solutions.solution import Solution
+
+class TS_SC_QBF(AbstractTS):
+    def __init__(self, tenure: int, iterations: int, filename: str):
+        self.fake = -1
+        super().__init__(SC_QBF_Inverse(filename), tenure, iterations)
+
+    def make_cl(self):
+        return list(range(self.obj_function.get_domain_size()))
+
+    def make_rcl(self):
+        return []
+
+    def make_tl(self):
+        return deque([self.fake] * (2 * self.tenure), maxlen=2 * self.tenure)
+
+    def update_cl(self):
+        pass
+
+    def create_empty_sol(self):
+        sol = Solution()
+        sol.cost = 0.0
+        return sol
+
+
+    def constructive_stop_criteria(self, cost):
+        return self.obj_function.is_feasible(self.sol) and cost <= self.sol.cost
+
+    def neighborhood_move(self):
+        min_delta_cost = float('inf')
+        best_cand_in = None
+        best_cand_out = None
+
+        # Evaluate insertions
+        for cand_in in self.cl:
+            delta_cost = self.obj_function.evaluate_insertion_cost(cand_in, self.sol)
+            if (cand_in not in self.tl) or (self.sol.cost + delta_cost < self.best_sol.cost):
+                if delta_cost < min_delta_cost:
+                    min_delta_cost = delta_cost
+                    best_cand_in = cand_in
+                    best_cand_out = None
+
+        # Evaluate removals
+        for cand_out in self.sol:
+            temp_sol = Solution(self.sol)
+            temp_sol.remove(cand_out)
+            if self.obj_function.is_feasible(temp_sol):
+                delta_cost = self.obj_function.evaluate_removal_cost(cand_out, self.sol)
+                if (cand_out not in self.tl) or (self.sol.cost + delta_cost < self.best_sol.cost):
+                    if delta_cost < min_delta_cost:
+                        min_delta_cost = delta_cost
+                        best_cand_in = None
+                        best_cand_out = cand_out
+
+        # Evaluate exchanges
+        for cand_in in self.cl:
+            for cand_out in self.sol:
+                temp_sol = Solution(self.sol)
+                temp_sol.append(cand_in)
+                temp_sol.remove(cand_out)
+                if self.obj_function.is_feasible(temp_sol):
+                    delta_cost = self.obj_function.evaluate_exchange_cost(cand_in, cand_out, self.sol)
+                    if ((cand_in not in self.tl) and (cand_out not in self.tl)) or \
+                    (self.sol.cost + delta_cost < self.best_sol.cost):
+                        if delta_cost < min_delta_cost:
+                            min_delta_cost = delta_cost
+                            best_cand_in = cand_in
+                            best_cand_out = cand_out
+
+        # Implement the best non-tabu move
+        if len(self.tl) >= 2 * self.tenure:
+            self.tl.popleft()
+        if best_cand_out is not None:
+            self.sol.remove(best_cand_out)
+            self.cl.append(best_cand_out)
+            self.tl.append(best_cand_out)
+        else:
+            self.tl.append(self.fake)
+        
+        if len(self.tl) >= 2 * self.tenure:
+            self.tl.popleft()
+        if best_cand_in is not None:
+            self.sol.append(best_cand_in)
+            self.cl.remove(best_cand_in)
+            self.tl.append(best_cand_in)
+        else:
+            self.tl.append(self.fake)
+
+        self.obj_function.evaluate(self.sol)
+
+    def solve(self):
+        self.best_sol = self.create_empty_sol()
+        self.constructive_heuristic()
+        self.tl = self.make_tl()
+        for i in range(self.iterations):
+            self.neighborhood_move()
+            if self.obj_function.is_feasible(self.sol) and self.best_sol.cost > self.sol.cost:
+                self.best_sol = Solution(self.sol)
+                if self.verbose:
+                    print(f"(Iter. {i}) BestSol = {self.best_sol}")
+
+        return self.best_sol
