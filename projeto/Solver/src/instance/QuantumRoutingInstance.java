1ab3f52bdb828b25e5de5653b33f6d07fac2d1ff
Gabrielle Barbosa <g233862@dac.unicamp.br>
2025-11-03 21:19:56 -0300

    read instance from file

--- projeto/Solver/src/instance/QuantumRoutingInstance.java
+++ projeto/Solver/src/instance/QuantumRoutingInstance.java
@@ -1,7 +1,13 @@
 package instance;
 
+import org.json.JSONArray;
 import utils.Pair;
+
+import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class QuantumRoutingInstance {
@@ -24,7 +30,49 @@ public class QuantumRoutingInstance {
     }
 
     public QuantumRoutingInstance(String filename) {
+        try {
+            String content = Files.readString(Paths.get(filename));
+            JSONObject json = new JSONObject(content);
 
+            this.requests = new ArrayList<>();
+            JSONArray requests = json.getJSONObject("graph").getJSONArray("sd_pairs");
+            for (int i = 0; i < requests.length(); i++) {
+                JSONArray pair = requests.getJSONArray(i);
+                int source = pair.getInt(0);
+                int destiny = pair.getInt(1);
+                this.requests.add(new Pair<>(source, destiny));
+            }
+
+            this.nodes = new ArrayList<>();
+            JSONArray nodes = json.getJSONArray("nodes");
+            for (int i = 0; i < nodes.length(); i++) {
+                JSONObject node = nodes.getJSONObject(i);
+                int memorySize = node.getInt("memory_size");
+                this.nodes.add(memorySize);
+            }
+
+            this.arcs = new ArrayList<>();
+            for (int i = 0; i < nodes.length(); i++) {
+                this.arcs.add(new ArrayList<>());
+                for (int j = 0; i < nodes.length(); i++) {
+                    this.arcs.get(i).add(null);
+                }
+            }
+
+            JSONArray arcs = json.getJSONArray("links");
+            for (int i = 0; i < arcs.length(); i++) {
+                JSONObject link = nodes.getJSONObject(i);
+                int source = link.getInt("source");
+                int target = link.getInt("target");
+                int channels = link.getInt("channels");
+                float prob = link.getInt("external_link_prob");
+
+                this.arcs.get(source).set(target, new Pair<>(channels, prob));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public int getSize() {
@@ -41,5 +89,32 @@ public class QuantumRoutingInstance {
 
     public List<Pair<Integer, Integer>> getRequests() {
         return requests;
+    }
+
+    @Override
+    public QuantumRoutingInstance clone() {
+        int newSize = this.size;
+
+        List<Integer> newNodes = new ArrayList<>(this.nodes);
+
+        List<List<Pair<Integer, Float>>> newArcs = new ArrayList<>();
+        for (List<Pair<Integer, Float>> row : this.arcs) {
+            List<Pair<Integer, Float>> newRow = new ArrayList<>();
+            for (Pair<Integer, Float> pair : row) {
+                if (pair == null) {
+                    newRow.add(null);
+                } else {
+                    newRow.add(new Pair<>(pair.getFirst(), pair.getSecond()));
+                }
+            }
+            newArcs.add(newRow);
+        }
+
+        List<Pair<Integer, Integer>> newRequests = new ArrayList<>();
+        for (Pair<Integer, Integer> req : this.requests) {
+            newRequests.add(new Pair<>(req.getFirst(), req.getSecond()));
+        }
+
+        return new QuantumRoutingInstance(newSize, newNodes, newArcs, newRequests);
     }
 }
