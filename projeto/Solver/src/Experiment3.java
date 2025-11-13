3fa1868bb565d31e24b99c34a8de4bd15cf219e7
Gabrielle Barbosa <g233862@dac.unicamp.br>
2025-11-12 21:34:42 -0300

    parallelize experiment

--- projeto/Solver/src/Experiment3.java
+++ projeto/Solver/src/Experiment3.java
@@ -7,53 +7,112 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Experiment3 {
-    public static void main(String[] args) throws Exception {
+    private static List<OptionsTS> setupConfigs() {
+        Random rng = new Random(1);
         List<String> instances = new ArrayList<>();
         File dataDir = new File("../instances/data/");
         File[] files = dataDir.listFiles((dir, name) -> name.endsWith(".json"));
 
         assert files != null;
-        for (File file : files) {
+        for (File file : Arrays.stream(files).sorted().toList()) {
             String fileName = file.getName();
             instances.add(fileName.substring(0, fileName.lastIndexOf(".")));
         }
 
+        List<OptionsTS> allConfigurations = new ArrayList<>();
 
-        List<OptionsTS> configurations = List.of(
-                new OptionsTS(1000, 1800, null, true, true, false, 0, 0, 1)
+        List<OptionsTS> performanceProfileConfigurations = List.of(
+                new OptionsTS("PP1",1000, 1800, null, true, true, false, 0, 0, 1),
+        new OptionsTS("PP2",1000, 1800, null, true, true, false, 0, 0, 1)
         );
 
-        try (FileWriter writer = new FileWriter("results.csv")) {
-            writer.write("Instance,Configuration,BestSol,Target,Time,Iteration\n");
+        List<OptionsTS> tttConfigurations = List.of(
+                new OptionsTS("TTT1", "instance_n300_sd10_0", 1000, 1800, 20, true, true, false, 0, 0, 1)
+        );
 
-            for (String inst : instances) {
-                int configIndex = 1;
-                for (OptionsTS config : configurations) {
-                    String configName = "CONFIG" + configIndex;
-                    System.out.println("Running instance: " + inst + " with config " + configName);
-                    QuantumRoutingInstance instance = new QuantumRoutingInstance("../instances/data/" + inst + ".json");
-                    QuantumRoutingTS solver = new QuantumRoutingTS(instance, 1, config);
-                    List<SolutionMetadata> results = solver.solve();
-
-                    StringBuilder csvLines = new StringBuilder();
-                    for (SolutionMetadata result : results) {
-                        csvLines.append(inst).append(",");
-                        csvLines.append(configName).append(",");
-                        csvLines.append(result.getSol().getCost()).append(",");
-                        csvLines.append(config.getTarget()).append(",");
-                        csvLines.append(result.getTime()).append(",");
-                        csvLines.append(result.getIterations()).append("\n");
-                    }
-                    writer.write(csvLines.toString());
-                    writer.flush();
-                    configIndex++;
-                }
+        for (String instance : instances) {
+            for (OptionsTS config : performanceProfileConfigurations) {
+                allConfigurations.add(new OptionsTS(config).setInstanceName(instance));
             }
+        }
+
+        for (OptionsTS config : tttConfigurations) {
+            for (int i = 0; i < 50; i++) {
+                allConfigurations.add(new OptionsTS(config).setRngSeed(rng.nextInt(1000)));
+            }
+        }
+
+        return allConfigurations;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<OptionsTS> configurations = setupConfigs();
+
+        int numThreads = Runtime.getRuntime().availableProcessors(); // Define the number of threads
+
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("results.csv");
+            writer.write("Instance,Configuration,BestSol,Target,TimeMS,Iteration\n");
+
+            final FileWriter finalWriter = writer;
+            for (OptionsTS config : configurations) {
+                Runnable task = () -> {
+                    try {
+                        System.out.println("Running instance: " + config.getInstanceName() + " with config " + config.getName());
+                        QuantumRoutingInstance instance = new QuantumRoutingInstance("../instances/data/" + config.getInstanceName() + ".json");
+                        QuantumRoutingTS solver = new QuantumRoutingTS(instance, 1, config);
+                        List<SolutionMetadata> results = solver.solve();
+
+                        StringBuilder csvLines = new StringBuilder();
+                        for (SolutionMetadata result : results) {
+                            csvLines.append(config.getInstanceName()).append(",");
+                            csvLines.append(config.getName()).append(",");
+                            csvLines.append(result.getSol().getCost()).append(",");
+                            csvLines.append(config.getTarget()).append(",");
+                            csvLines.append(result.getTime()).append(",");
+                            csvLines.append(result.getIterations()).append("\n");
+                        }
+                        synchronized (finalWriter) {
+                            finalWriter.write(csvLines.toString());
+                            finalWriter.flush();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                };
+                executor.submit(task);
+            }
+
         } catch (IOException e) {
             throw new Exception(e);
+        } finally {
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(1, TimeUnit.HOURS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- projeto/Solver/src/metaheuristics/OptionsTS.java
+++ projeto/Solver/src/metaheuristics/OptionsTS.java
@@ -1,6 +1,8 @@
 package metaheuristics;
 
 public class OptionsTS {
+    protected String name;
+    protected String instanceName;
     protected boolean adaptiveTL = false;
     protected boolean removeEnabled = true;
     protected boolean exchangeEnabled = true;
@@ -11,8 +13,23 @@ public class OptionsTS {
     protected Integer iterations = 1000;
     protected Integer target = null;
 
-    public OptionsTS(int iterations, int timeoutSeconds, Integer target, boolean removeEnabled, boolean exchangeEnabled,
+    public OptionsTS(OptionsTS opts) {
+        this.name = opts.name;
+        this.iterations = opts.iterations;
+        this.timeoutSeconds = opts.timeoutSeconds;
+        this.target = opts.target;
+        this.removeEnabled = opts.removeEnabled;
+        this.exchangeEnabled = opts.exchangeEnabled;
+        this.adaptiveTL = opts.adaptiveTL;
+        this.intensifyRate = opts.intensifyRate;
+        this.diversifyRate = opts.diversifyRate;
+        this.rngSeed = opts.rngSeed;
+        this.instanceName = opts.instanceName;
+    }
+
+    public OptionsTS(String name, int iterations, int timeoutSeconds, Integer target, boolean removeEnabled, boolean exchangeEnabled,
                      boolean adaptiveTL, float intensifyRate, float diversifyRate, int rngSeed) {
+        this.name = name;
         this.iterations = iterations;
         this.timeoutSeconds = timeoutSeconds;
         this.target = target;
@@ -22,77 +39,119 @@ public class OptionsTS {
         this.intensifyRate = intensifyRate;
         this.diversifyRate = diversifyRate;
         this.rngSeed = rngSeed;
+    }
+
+    public OptionsTS(String name, String instanceName, int iterations, int timeoutSeconds, Integer target, boolean removeEnabled, boolean exchangeEnabled,
+                     boolean adaptiveTL, float intensifyRate, float diversifyRate, int rngSeed) {
+        this.name = name;
+        this.iterations = iterations;
+        this.timeoutSeconds = timeoutSeconds;
+        this.target = target;
+        this.removeEnabled = removeEnabled;
+        this.exchangeEnabled = exchangeEnabled;
+        this.adaptiveTL = adaptiveTL;
+        this.intensifyRate = intensifyRate;
+        this.diversifyRate = diversifyRate;
+        this.rngSeed = rngSeed;
+        this.instanceName = instanceName;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public OptionsTS setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public OptionsTS setName(String name) {
+        this.name = name;
+        return this;
     }
 
     public boolean isAdaptiveTL() {
         return adaptiveTL;
     }
 
-    public void setAdaptiveTL(boolean adaptiveTL) {
+    public OptionsTS setAdaptiveTL(boolean adaptiveTL) {
         this.adaptiveTL = adaptiveTL;
+        return this;
     }
 
     public boolean isRemoveEnabled() {
         return removeEnabled;
     }
 
-    public void setRemoveEnabled(boolean removeEnabled) {
+    public OptionsTS setRemoveEnabled(boolean removeEnabled) {
         this.removeEnabled = removeEnabled;
+        return this;
     }
 
     public boolean isExchangeEnabled() {
         return exchangeEnabled;
     }
 
-    public void setExchangeEnabled(boolean exchangeEnabled) {
+    public OptionsTS setExchangeEnabled(boolean exchangeEnabled) {
         this.exchangeEnabled = exchangeEnabled;
+        return this;
     }
 
     public float getIntensifyRate() {
         return intensifyRate;
     }
 
-    public void setIntensifyRate(float intensifyRate) {
+    public OptionsTS setIntensifyRate(float intensifyRate) {
         this.intensifyRate = intensifyRate;
+        return this;
     }
 
     public float getDiversifyRate() {
         return diversifyRate;
     }
 
-    public void setDiversifyRate(float diversifyRate) {
+    public OptionsTS setDiversifyRate(float diversifyRate) {
         this.diversifyRate = diversifyRate;
+        return this;
     }
 
     public int getRngSeed() {
         return rngSeed;
     }
 
-    public void setRngSeed(int rngSeed) {
+    public OptionsTS setRngSeed(int rngSeed) {
         this.rngSeed = rngSeed;
+        return this;
     }
 
     public int getTimeoutSeconds() {
         return timeoutSeconds;
     }
 
-    public void setTimeoutSeconds(int timeoutSeconds) {
+    public OptionsTS setTimeoutSeconds(int timeoutSeconds) {
         this.timeoutSeconds = timeoutSeconds;
+        return this;
     }
 
     public Integer getIterations() {
         return iterations;
     }
 
-    public void setIterations(Integer iterations) {
+    public OptionsTS setIterations(Integer iterations) {
         this.iterations = iterations;
+        return this;
     }
 
     public Integer getTarget() {
         return target;
     }
 
-    public void setTarget(Integer target) {
+    public OptionsTS setTarget(Integer target) {
         this.target = target;
+        return this;
     }
 }
